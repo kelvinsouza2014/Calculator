660be670cd7947cd07a161fc6781dae26be38ec8
Kelvin Souza <kelvinsouza2017@hotmail.com>
2026-04-24 16:11:06 -0300

    feat: Added attributes + constructor() + getters() + setters()

--- src/br/com/calculator/model/Calculator.java
+++ src/br/com/calculator/model/Calculator.java
@@ -1,5 +1,35 @@
 package br.com.calculator.model;
 
 public class Calculator {
+    private double num1;
+    private double num2;
+    private double result;
 
+    Calculator(){
+
+    }
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
 }
