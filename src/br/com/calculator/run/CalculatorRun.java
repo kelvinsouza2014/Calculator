f328e44a78db4c4b42a54fe4953898f44229bb30
Kelvin Souza <kelvinsouza2017@hotmail.com>
2026-04-24 16:20:55 -0300

    feat: Instantiation Calculator()

--- src/br/com/calculator/run/CalculatorRun.java
+++ src/br/com/calculator/run/CalculatorRun.java
@@ -1,9 +1,11 @@
 package br.com.calculator.run;
 
+import br.com.calculator.model.Calculator;
+
 import java.util.Scanner;
 
 public class CalculatorRun {
     Scanner sc = new Scanner(System.in);
 
-
+    Calculator calculator = new Calculator();
 }
