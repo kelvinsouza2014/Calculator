e55601652dea6271d068fbbcce0647e87619c857
Kelvin Souza <kelvinsouza2017@hotmail.com>
2026-04-27 14:39:22 -0300

    feat: Methods - getNum1 / getOperator / getNum2

--- src/br/com/calculator/run/CalculatorRun.java
+++ src/br/com/calculator/run/CalculatorRun.java
@@ -4,8 +4,33 @@ import br.com.calculator.model.Calculator;
 
 import java.util.Scanner;
 
-public class CalculatorRun {
-    Scanner sc = new Scanner(System.in);
 
-    Calculator calculator = new Calculator();
+public class CalculatorRun {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Calculator calculator = new Calculator();
+        calculator.setNum1(getNum1(sc));
+        calculator.setOperator(getOperator(sc));
+        calculator.setNum2(getNum2(sc));
+
+    }
+
+    public static double getNum1(Scanner sc) {
+        System.out.print("Digite o primeiro valor: ");
+        double tNum1 = sc.nextDouble();
+        return tNum1;
+    }
+
+    public static String getOperator(Scanner sc) {
+        System.out.print("Digite o operator: ");
+        String tOperator = sc.next();
+        return tOperator;
+    }
+
+    public static double getNum2(Scanner sc) {
+        System.out.print("Digite o segundo número: ");
+        double tNum2 = sc.nextDouble();
+        return tNum2;
+    }
 }
